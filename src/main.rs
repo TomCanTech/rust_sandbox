2738c5632b99cc5ab998a0a319925aa6bf224f15
TomCanTech <tomcantech@gmail.com>
2024-08-16 00:50:15 +0100

    Test commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", fibonacci(45));
+}
+
+fn fibonacci(size: u128) -> u128 {
+    if size < 2 {
+        return 1;
+    }
+    return fibonacci(size-1) + fibonacci(size-2);
 }
