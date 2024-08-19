9fdbf2912d38b8f222574565ca3a8159c8528303
Thomas Reed <tomcantech@gmail.com>
2024-08-19 22:22:18 +0100

    Made enums

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,36 @@ fn main() {
         width: request_length("width"),
         height: request_length("height")
     };
-    println!("The rectangle area is equal to {}", shape.width*shape.height);
+    let shape_variation = CuboidVar::return_variation(&shape);
+
+    println!("The shape's variation is a {}.", 
+        match shape_variation {
+            CuboidVar::Square => "square",
+            CuboidVar::Rectangle => "rectangle" 
+        }
+    );
 }
 struct Cuboid{
     width: u32,
     height: u32
 }
+
+impl Cuboid{
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: Cuboid) -> bool {
+        (self.width > other.width && self.height > other.height)
+        || (self.width > other.height && self.height > other.width)
+    }
+    fn square(length: u32) -> Self {
+        Self {
+            width: length,
+            height: length
+        }
+    }
+}
+
 
 fn request_length(side: &str) -> u32 {
     let mut side_str = String::new();
@@ -31,4 +55,19 @@ fn request_length(side: &str) -> u32 {
         break
     }
     side_val
+}
+
+enum CuboidVar {
+    Square,
+    Rectangle
+}
+
+impl CuboidVar {
+    fn return_variation(shape: &Cuboid) -> Self {
+        if shape.width == shape.height {
+            return Self::Square
+        } else {
+            return Self::Rectangle
+        }
+    }
 }
